afcc5747e0301f285b3453503b747bb75e189e7c
Artem Prokofiev <artem-prokofiev@yandex.ru>
2024-02-20 15:18:18 +0400

    added lab 2.2

---
+++ 2.2_EF_lab/AppConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2_EF_lab
+{
+    internal class AppConfiguration : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            string connectionString = Program.configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder.LogTo(s => Debug.WriteLine(s));
+        }
+    }
+}
