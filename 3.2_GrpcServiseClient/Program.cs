f1dddc973eed2c07f292fcdcb44650881f447d25
Artem Prokofiev <artem-prokofiev@yandex.ru>
2024-02-27 12:56:17 +0400

    added Grpc client

---
+++ 3.2_GrpcServiseClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using GrpcServiceClient;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5035");
+
+var client = new CoursesService.CoursesServiceClient(channel);
+
+var replayCourses = await client.ListCoursesAsync(new Google.Protobuf.WellKnownTypes.Empty());
+
+foreach(var course in replayCourses.Courses)
+    Console.WriteLine($"Course - {course.Id}.{course.Title} with duration {course.Duration} - {course.Description}\n");
