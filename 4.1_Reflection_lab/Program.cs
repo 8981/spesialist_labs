4f57168dd51c2d7e9a4bd01c2f937e0e453bba21
Artem Prokofiev <artem-prokofiev@yandex.ru>
2024-03-02 15:33:57 +0400

    added logics for information about class lib

--- 4.1_Reflection_lab/Program.cs
+++ 4.1_Reflection_lab/Program.cs
@@ -9,11 +9,22 @@ namespace _4._1_Reflection_lab
             Assembly lib = Assembly.LoadFrom("4.1_ReflectionLib_lab.dll");
             Type[] types = lib.GetTypes();
 
-            foreach (Type type in types)
+
+            foreach (Type t in types)
             {
-                if (type.IsClass)
+                TypeInfo typeInfo = t.GetTypeInfo();
+                var classModifire = typeInfo.Attributes;
+                
+                if (t.IsClass)
                 {
-                    Console.WriteLine(type.FullName);
+                    Console.WriteLine($"{t.Name}: class modifire {classModifire}");
+                    PropertyInfo[] properties = t.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+                    foreach (PropertyInfo prop in properties)
+                    {
+                        var modifiers = prop.GetAccessors()[0].Attributes;
+                        Console.WriteLine($"  Property: {prop.Name}, Modifiers: {modifiers}");
+                    }
                 }
             }
         }
